515d23738af3084dabdc4c12545dfbc1d7ae4a47
phdang1 <phdang1@.edu>
2015-11-30 18:43:53 -0800

    Implement notify player numbers. Delete Client Input/Output Threads

--- src/infrastructure/GameServer.java
+++ src/infrastructure/GameServer.java
@@ -1,5 +1,6 @@
 package infrastructure;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -22,10 +23,30 @@ public class GameServer {
 				Socket player2 = server.accept();
 				System.out.println("Player 2 connected");
 				
+				notifyPlayerNumbers(player1, player2);
+				
 				new Thread(new GameThread(player1, player2)).start();
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+	}
+	
+	/**
+	 * Notifies the players connected to sockets p1 and p2 which player number
+	 * they are
+	 * 
+	 * @param p1 player 1's socket
+	 * @param p2 player 2's socket
+	 * 
+	 * @throws IOException on IO errors
+	 */
+	private static void notifyPlayerNumbers(Socket p1, Socket p2) throws IOException {
+		DataOutputStream outToP1 = new DataOutputStream(p1.getOutputStream());
+		DataOutputStream outToP2 = new DataOutputStream(p2.getOutputStream());
+		
+		// arbitrary decision
+		outToP1.writeBoolean(true);
+		outToP2.writeBoolean(false);
 	}
 }
