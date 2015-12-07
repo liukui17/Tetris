ed832651d61362d283bb9956cc36d21b01cfe651
mkim123 <tie23he@gmail.com>
2015-12-06 16:21:29 -0800

    made menuPanel less ugly

--- src/gui/MenuPanel.java
+++ src/gui/MenuPanel.java
@@ -1,18 +1,21 @@
 package gui;
 
 import java.awt.Color;
+import java.awt.Component;
+import java.awt.Dimension;
 import java.awt.Font;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.Box;
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.KeyStroke;
 import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
 
 public class MenuPanel extends JPanel implements ActionListener {
 	private ButtonListener buttonListener;
@@ -20,43 +23,53 @@ public class MenuPanel extends JPanel implements ActionListener {
 	public MenuPanel() {
 
 		// Set Layout Manager
-		GridBagLayout layout = new GridBagLayout();
-		setLayout(layout);
-
-		GridBagConstraints gbc = new GridBagConstraints();
-		gbc.weightx = 1;
-		gbc.weighty = 1;
+		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		setBorder(new EmptyBorder(new Insets(30, 30, 30, 30)));
+		setBackground(Color.LIGHT_GRAY);
 
 		// Create & Add Swing Components
 		JLabel title = new JLabel("Tetris");
 		title.setFont(new Font("Dialog", Font.BOLD, 50));
 		title.setHorizontalAlignment(SwingConstants.CENTER);
-		gbc.fill = GridBagConstraints.BOTH;
-		gbc.gridx = 0;
-		gbc.gridy = 0;
-		gbc.insets = new Insets(20, 20, 20, 20);
-		add(title, gbc);
+		title.setAlignmentX(Component.CENTER_ALIGNMENT);
+		add(title);
+		
+		// Filler
+		add(Box.createRigidArea(new Dimension(0, 200)));
+		add(Box.createVerticalGlue());
 
 		JButton options = new JButton("Options");
+		options.setFont(new Font("Dialog", Font.PLAIN, 40));
+		options.setAlignmentX(Component.CENTER_ALIGNMENT);
 		options.addActionListener(this);
 		options.getInputMap().put(KeyStroke.getKeyStroke("SPACE"), "none");
-		gbc.gridx = 0;
-		gbc.gridy = 1;
-		add(options, gbc);
+		
+		add(options);
+		
+		// Filler
+		add(Box.createRigidArea(new Dimension(0, 50)));
 
 		JButton help = new JButton("Help");
+		help.setFont(new Font("Dialog", Font.PLAIN, 40));
+		help.setAlignmentX(Component.CENTER_ALIGNMENT);
 		help.addActionListener(this);
-		gbc.gridx = 0;
-		gbc.gridy = 2;
-		add(help, gbc);
+		add(help);
+		
+		// Filler
+		add(Box.createRigidArea(new Dimension(0, 50)));
 
 		JButton start = new JButton("Start");
+		start.setFont(new Font("Dialog", Font.PLAIN, 40));
+		start.setAlignmentX(Component.CENTER_ALIGNMENT);
 		start.addActionListener(this);
-		gbc.gridx = 0;
-		gbc.gridy = 3;
-		add(start, gbc);
+		add(start);
+		
 
-		setBackground(Color.LIGHT_GRAY);
+		int maxWidth = Math.max(Math.max((int)options.getPreferredSize().getWidth(), (int)help.getPreferredSize().getWidth()), (int)start.getPreferredSize().getWidth());
+		Dimension buttonDim = new Dimension(maxWidth, 0);
+		options.setMaximumSize(buttonDim);
+		help.setMaximumSize(buttonDim);
+		start.setMaximumSize(buttonDim);
 	}
 
 	public void actionPerformed(ActionEvent e) {
